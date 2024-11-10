8c355c5867b5b3285c93d4dcea4f20eeabedf000
Mateusz Cegiełka <mateusz@cegla.net>
2024-11-10 21:05:44 +0100

    Remove unused code in task 2

--- task_2.c
+++ task_2.c
@@ -3,10 +3,6 @@
 #include <stm32.h>
 #include <string.h>
 
-#define RED_LED_GPIO GPIOA
-#define GREEN_LED_GPIO GPIOA
-#define BLUE_LED_GPIO GPIOB
-#define GREEN2_LED_GPIO GPIOA
 #define LEFT_BUTTON_GPIO GPIOB
 #define RIGHT_BUTTON_GPIO GPIOB
 #define UP_BUTTON_GPIO GPIOB
@@ -15,10 +11,6 @@
 #define USER_BUTTON_GPIO GPIOC
 #define MODE_BUTTON_GPIO GPIOA
 
-#define RED_LED_PIN 6
-#define GREEN_LED_PIN 7
-#define BLUE_LED_PIN 0
-#define GREEN2_LED_PIN 5
 #define LEFT_BUTTON_PIN 3
 #define RIGHT_BUTTON_PIN 4
 #define UP_BUTTON_PIN 5
@@ -27,22 +19,6 @@
 #define USER_BUTTON_PIN 13
 #define MODE_BUTTON_PIN 0
 
-#define RedLEDon() RED_LED_GPIO->BSRR = 1 << (RED_LED_PIN + 16)
-#define RedLEDoff() RED_LED_GPIO->BSRR = 1 << RED_LED_PIN
-#define RedLEDstatus() ((RED_LED_GPIO->ODR & (1 << RED_LED_PIN)) == 0)
-
-#define GreenLEDon() GREEN_LED_GPIO->BSRR = 1 << (GREEN_LED_PIN + 16)
-#define GreenLEDoff() GREEN_LED_GPIO->BSRR = 1 << GREEN_LED_PIN
-#define GreenLEDstatus() ((GREEN_LED_GPIO->ODR & (1 << GREEN_LED_PIN)) == 0)
-
-#define BlueLEDon() BLUE_LED_GPIO->BSRR = 1 << (BLUE_LED_PIN + 16)
-#define BlueLEDoff() BLUE_LED_GPIO->BSRR = 1 << BLUE_LED_PIN
-#define BlueLEDstatus() ((BLUE_LED_GPIO->ODR & (1 << BLUE_LED_PIN)) == 0)
-
-#define Green2LEDon() GREEN2_LED_GPIO->BSRR = 1 << GREEN2_LED_PIN
-#define Green2LEDoff() GREEN2_LED_GPIO->BSRR = 1 << (GREEN2_LED_PIN + 16)
-#define Green2LEDstatus() ((GREEN2_LED_GPIO->ODR & (1 << GREEN2_LED_PIN)) != 0)
-
 #define LeftButtonStatus() ((LEFT_BUTTON_GPIO->IDR & (1 << LEFT_BUTTON_PIN)) == 0)
 #define RightButtonStatus() ((RIGHT_BUTTON_GPIO->IDR & (1 << RIGHT_BUTTON_PIN)) == 0)
 #define UpButtonStatus() ((UP_BUTTON_GPIO->IDR & (1 << UP_BUTTON_PIN)) == 0)
@@ -50,27 +26,6 @@
 #define FireButtonStatus() ((FIRE_BUTTON_GPIO->IDR & (1 << FIRE_BUTTON_PIN)) == 0)
 #define UserButtonStatus() ((USER_BUTTON_GPIO->IDR & (1 << USER_BUTTON_PIN)) == 0)
 #define ModeButtonStatus() ((MODE_BUTTON_GPIO->IDR & (1 << MODE_BUTTON_PIN)) != 0)
-
-#define USART_Mode_Rx_Tx (USART_CR1_RE | USART_CR1_TE)
-#define USART_Enable USART_CR1_UE
-
-#define USART_WordLength_8b 0x0000
-#define USART_WordLength_9b USART_CR1_M
-
-#define USART_Parity_No 0x0000
-#define USART_Parity_Even USART_CR1_PCE
-#define USART_Parity_Odd (USART_CR1_PCE | USART_CR1_PS)
-
-#define USART_StopBits_1 0x0000
-#define USART_StopBits_0_5 0x1000
-#define USART_StopBits_2 0x2000
-#define USART_StopBits_1_5 0x3000
-
-#define USART_FlowControl_None 0x0000
-#define USART_FlowControl_RTS USART_CR3_RTSE
-#define USART_FlowControl_CTS USART_CR3_CTSE
-
-#define USART_DMA_Rx_Tx (USART_CR3_DMAT | USART_CR3_DMAR)
 
 #define HSI_HZ 16000000U
 #define PCLK1_HZ HSI_HZ
@@ -159,59 +114,6 @@ int main() {
 
     __NOP();
 
-    RedLEDoff();
-    GreenLEDoff();
-    BlueLEDoff();
-    Green2LEDoff();
-
-    RedLEDon();
-    BlueLEDon();
-
-    GPIOoutConfigure(RED_LED_GPIO,
-        RED_LED_PIN,
-        GPIO_OType_PP,
-        GPIO_Low_Speed,
-        GPIO_PuPd_NOPULL);
-    GPIOoutConfigure(GREEN_LED_GPIO,
-        GREEN_LED_PIN,
-        GPIO_OType_PP,
-        GPIO_Low_Speed,
-        GPIO_PuPd_NOPULL);
-    GPIOoutConfigure(BLUE_LED_GPIO,
-        BLUE_LED_PIN,
-        GPIO_OType_PP,
-        GPIO_Low_Speed,
-        GPIO_PuPd_NOPULL);
-    GPIOoutConfigure(GREEN2_LED_GPIO,
-        GREEN2_LED_PIN,
-        GPIO_OType_PP,
-        GPIO_Low_Speed,
-        GPIO_PuPd_NOPULL);
-
-    GPIOafConfigure(GPIOA,
-        2,
-        GPIO_OType_PP,
-        GPIO_Fast_Speed,
-        GPIO_PuPd_NOPULL,
-        GPIO_AF_USART2);
-    GPIOafConfigure(GPIOA,
-        3,
-        GPIO_OType_PP,
-        GPIO_Fast_Speed,
-        GPIO_PuPd_UP,
-        GPIO_AF_USART2);
-
-    USART2->CR1 = USART_Mode_Rx_Tx | USART_WordLength_8b | USART_Parity_No;
-    USART2->CR2 = USART_StopBits_1;
-    USART2->CR3 = USART_FlowControl_None | USART_DMA_Rx_Tx;
-    USART2->BRR = (PCLK1_HZ + (BAUD / 2U)) / BAUD;
-
-    DMA1_Stream6->CR = 4u << 25 | DMA_SxCR_PL_1 | DMA_SxCR_MINC | DMA_SxCR_DIR_0 | DMA_SxCR_TCIE;
-    DMA1_Stream6->PAR = (uint32_t) &USART2->DR;
-    DMA1->HIFCR = DMA_HIFCR_CTCIF6;
-    NVIC_EnableIRQ(DMA1_Stream6_IRQn);
-    USART2->CR1 |= USART_Enable;
-
     GPIOinConfigure(LEFT_BUTTON_GPIO, LEFT_BUTTON_PIN, GPIO_PuPd_UP, EXTI_Mode_Interrupt, EXTI_Trigger_Rising_Falling);
     GPIOinConfigure(UP_BUTTON_GPIO, UP_BUTTON_PIN, GPIO_PuPd_UP, EXTI_Mode_Interrupt, EXTI_Trigger_Rising_Falling);
     GPIOinConfigure(RIGHT_BUTTON_GPIO, RIGHT_BUTTON_PIN, GPIO_PuPd_UP, EXTI_Mode_Interrupt, EXTI_Trigger_Rising_Falling);
@@ -219,20 +121,36 @@ int main() {
     GPIOinConfigure(FIRE_BUTTON_GPIO, FIRE_BUTTON_PIN, GPIO_PuPd_UP, EXTI_Mode_Interrupt, EXTI_Trigger_Rising_Falling);
     GPIOinConfigure(USER_BUTTON_GPIO, USER_BUTTON_PIN, GPIO_PuPd_UP, EXTI_Mode_Interrupt, EXTI_Trigger_Rising_Falling);
     GPIOinConfigure(MODE_BUTTON_GPIO, MODE_BUTTON_PIN, GPIO_PuPd_UP, EXTI_Mode_Interrupt, EXTI_Trigger_Rising_Falling);
+
     NVIC_EnableIRQ(EXTI3_IRQn);
     NVIC_EnableIRQ(EXTI4_IRQn);
     NVIC_EnableIRQ(EXTI9_5_IRQn);
     NVIC_EnableIRQ(EXTI15_10_IRQn);
     NVIC_EnableIRQ(EXTI0_IRQn);
 
+    GPIOafConfigure(GPIOA,
+        2,
+        GPIO_OType_PP,
+        GPIO_Fast_Speed,
+        GPIO_PuPd_NOPULL,
+        GPIO_AF_USART2);
+
+    USART2->CR1 = USART_CR1_TE;
+    USART2->CR2 = 0;
+    USART2->CR3 = USART_CR3_DMAT;
+    USART2->BRR = (PCLK1_HZ + (BAUD / 2U)) / BAUD;
+
+    DMA1_Stream6->CR = 4u << 25 | DMA_SxCR_PL_1 | DMA_SxCR_MINC | DMA_SxCR_DIR_0 | DMA_SxCR_TCIE;
+    DMA1_Stream6->PAR = (uint32_t) &USART2->DR;
+    DMA1->HIFCR = DMA_HIFCR_CTCIF6;
+
+    NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+
+    USART2->CR1 |= USART_CR1_UE;
+
     for (;;) {
         __NOP();
     }
-}
-
-void EXTI0_IRQHandler(void) {
-    EXTI->PR = EXTI_PR_PR0;
-    log_buffer_process_button(6, ModeButtonStatus());
 }
 
 void EXTI3_IRQHandler(void) {
@@ -255,6 +173,11 @@ void EXTI15_10_IRQHandler(void) {
     EXTI->PR = EXTI_PR_PR10 | EXTI_PR_PR13;
     log_buffer_process_button(4, FireButtonStatus());
     log_buffer_process_button(5, UserButtonStatus());
+}
+
+void EXTI0_IRQHandler(void) {
+    EXTI->PR = EXTI_PR_PR0;
+    log_buffer_process_button(6, ModeButtonStatus());
 }
 
 void DMA1_Stream6_IRQHandler() {
