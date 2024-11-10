172bf267dcdc4821e0435190fd19871a412112a6
Mateusz Cegiełka <mateusz@cegla.net>
2024-11-10 21:05:44 +0100

    Add using UART

--- leds_main.c
+++ leds_main.c
@@ -24,8 +24,32 @@
 #define Green2LEDon() GREEN2_LED_GPIO->BSRR = 1 << GREEN2_LED_PIN
 #define Green2LEDoff() GREEN2_LED_GPIO->BSRR = 1 << (GREEN2_LED_PIN + 16)
 
+#define USART_Mode_Rx_Tx (USART_CR1_RE | USART_CR1_TE)
+#define USART_Enable USART_CR1_UE
+
+#define USART_WordLength_8b 0x0000
+#define USART_WordLength_9b USART_CR1_M
+
+#define USART_Parity_No 0x0000
+#define USART_Parity_Even USART_CR1_PCE
+#define USART_Parity_Odd (USART_CR1_PCE | USART_CR1_PS)
+
+#define USART_StopBits_1 0x0000
+#define USART_StopBits_0_5 0x1000
+#define USART_StopBits_2 0x2000
+#define USART_StopBits_1_5 0x3000
+
+#define USART_FlowControl_None 0x0000
+#define USART_FlowControl_RTS USART_CR3_RTSE
+#define USART_FlowControl_CTS USART_CR3_CTSE
+
+#define HSI_HZ 16000000U
+#define PCLK1_HZ HSI_HZ
+#define BAUD 9600U
+
 int main() {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN;
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
     __NOP();
 
@@ -55,21 +79,34 @@ int main() {
         GPIO_Low_Speed,
         GPIO_PuPd_NOPULL);
 
+    GPIOafConfigure(GPIOA,
+        2,
+        GPIO_OType_PP,
+        GPIO_Fast_Speed,
+        GPIO_PuPd_NOPULL,
+        GPIO_AF_USART2);
+    GPIOafConfigure(GPIOA,
+        3,
+        GPIO_OType_PP,
+        GPIO_Fast_Speed,
+        GPIO_PuPd_UP,
+        GPIO_AF_USART2);
+
+    USART2->CR1 = USART_Mode_Rx_Tx | USART_WordLength_8b | USART_Parity_No;
+    USART2->CR2 = USART_StopBits_1;
+    USART2->CR3 = USART_FlowControl_None;
+    USART2->BRR = (PCLK1_HZ + (BAUD / 2U)) / BAUD;
+
+    USART2->CR1 |= USART_Enable;
+
     for (;;) {
-        RedLEDon();
-        Delay(4000000);
-        RedLEDoff();
-
-        GreenLEDon();
-        Delay(4000000);
-        GreenLEDoff();
-
-        BlueLEDon();
-        Delay(4000000);
-        BlueLEDoff();
-
-        Green2LEDon();
-        Delay(4000000);
-        Green2LEDoff();
+        if (USART2->SR & USART_SR_RXNE) {
+            char received_char = USART2->DR;
+            RedLEDon();
+            BlueLEDon();
+            Delay(100000);
+            RedLEDoff();
+            BlueLEDoff();
+        }
     }
 }
