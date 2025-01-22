7905715177ebc85318e08c0624a18a9efd12b18c
Mateusz Cegiełka <mateusz@cegla.net>
2025-01-22 16:21:28 +0100

    Add PWM control of all LEDs

--- task_3.c
+++ task_3.c
@@ -6,26 +6,10 @@
 #define RED_LED_GPIO GPIOA
 #define GREEN_LED_GPIO GPIOA
 #define BLUE_LED_GPIO GPIOB
-#define MODE_BUTTON_GPIO GPIOA
 
 #define RED_LED_PIN 6
 #define GREEN_LED_PIN 7
 #define BLUE_LED_PIN 0
-#define MODE_BUTTON_PIN 0
-
-#define RedLEDon() RED_LED_GPIO->BSRR = 1 << (RED_LED_PIN + 16)
-#define RedLEDoff() RED_LED_GPIO->BSRR = 1 << RED_LED_PIN
-#define RedLEDstatus() ((RED_LED_GPIO->ODR & (1 << RED_LED_PIN)) == 0)
-
-#define GreenLEDon() GREEN_LED_GPIO->BSRR = 1 << (GREEN_LED_PIN + 16)
-#define GreenLEDoff() GREEN_LED_GPIO->BSRR = 1 << GREEN_LED_PIN
-#define GreenLEDstatus() ((GREEN_LED_GPIO->ODR & (1 << GREEN_LED_PIN)) == 0)
-
-#define BlueLEDon() BLUE_LED_GPIO->BSRR = 1 << (BLUE_LED_PIN + 16)
-#define BlueLEDoff() BLUE_LED_GPIO->BSRR = 1 << BLUE_LED_PIN
-#define BlueLEDstatus() ((BLUE_LED_GPIO->ODR & (1 << BLUE_LED_PIN)) == 0)
-
-#define ModeButtonStatus() ((MODE_BUTTON_GPIO->IDR & (1 << MODE_BUTTON_PIN)) != 0)
 
 int main() {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN;
@@ -33,43 +17,26 @@ int main() {
 
     __NOP();
 
-    RedLEDoff();
-    GreenLEDoff();
-    BlueLEDoff();
+    GPIOafConfigure(RED_LED_GPIO, RED_LED_PIN, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
+    GPIOafConfigure(GREEN_LED_GPIO, GREEN_LED_PIN, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
+    GPIOafConfigure(BLUE_LED_GPIO, BLUE_LED_PIN, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
 
-    GPIOoutConfigure(RED_LED_GPIO, RED_LED_PIN, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL);
-    GPIOoutConfigure(GREEN_LED_GPIO, GREEN_LED_PIN, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL);
-    GPIOoutConfigure(BLUE_LED_GPIO, BLUE_LED_PIN, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL);
-
-    GPIOafConfigure(GPIOA, 6, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
-    GPIOafConfigure(GPIOA, 7, GPIO_OType_PP, GPIO_Low_Speed, GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
-
-    // TIM3->PSC = 63999;
-    TIM3->PSC = 1;
-    TIM3->ARR = 749;
+    TIM3->PSC = 10000;
+    TIM3->ARR = 20;
     TIM3->EGR = TIM_EGR_UG;
-    TIM3->CCR1 = 2;
-    TIM3->CCR2 = 748;
-    TIM3->CCMR1 = TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1
-        | TIM_CCMR1_OC1PE
-        | TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_0
-        | TIM_CCMR1_OC2PE;
-    TIM3->CCER = TIM_CCER_CC1E | TIM_CCER_CC1P | TIM_CCER_CC2E | TIM_CCER_CC2P;
+    TIM3->CCR1 = 1;
+    TIM3->CCR2 = 1;
+    TIM3->CCR3 = 1;
+    TIM3->CCMR1 = TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1PE
+        | TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2PE;
+    TIM3->CCMR2 = TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3PE;
+    TIM3->CCER = TIM_CCER_CC1E | TIM_CCER_CC1P
+        | TIM_CCER_CC2E | TIM_CCER_CC2P
+        | TIM_CCER_CC3E | TIM_CCER_CC3P;
     TIM3->CR1 = TIM_CR1_ARPE | TIM_CR1_CEN;
-
-    // GPIOinConfigure(MODE_BUTTON_GPIO, MODE_BUTTON_PIN, GPIO_PuPd_UP, EXTI_Mode_Interrupt, EXTI_Trigger_Rising_Falling);
-    // NVIC_EnableIRQ(EXTI0_IRQn);
 
     for (;;) {
         __NOP();
     }
 }
 
-// void EXTI0_IRQHandler(void) {
-//     EXTI->PR = EXTI_PR_PR0;
-//     if (ModeButtonStatus()) {
-//         RedLEDon();
-//     } else {
-//         RedLEDoff();
-//     }
-// }
