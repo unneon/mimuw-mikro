fa4a087ed5c694da1fc06b4820ba2b4fc98dd68e
Mateusz Cegiełka <mateusz@cegla.net>
2025-01-27 15:42:09 +0100

    Document click parameters

--- src/config.h
+++ src/config.h
@@ -1,20 +1,21 @@
 #pragma once
 
-// The minimal acceleration along X, Y or Z axis required to register a click,
-// measured in milli-g (1/1000th of 9.81m/s^2). Must range from 0.5g to 7.5g
-// with a step of 0.5g.
+// The threshold of acceleration along X, Y or Z axis required to start and
+// finish registering a click, measured in milli-g (1/1000th of 9.81m/s^2).
+// Must range from 0.5g to 7.5g with a step of 0.5g.
 #define CONFIG_CLICK_THRESHOLD_MG 500
 
-// TODO: Document how do these options exactly work?
-
-// "Time limit" between clicks, as (not) defined by LIS35DE documentation. Must
-// range from 0ms to 127.5ms with a step of 0.5ms.
+// The maximum time between the acceleration exceeding the threshold value and
+// acceleration then falling below the threshold value required to register any
+// click event. Must range from 0ms to 127.5ms with a step of 0.5ms.
 #define CONFIG_CLICK_TIMELIMIT_US 127'500
 
-// "Latency" between clicks, as (not) defined by LIS35DE documentation. Must
-// range from 0ms to 255ms with a step of 1ms.
+// The minimal duration after which a double click event can be registered
+// after a single click event. Must range from 0ms to 255ms with a step of 1ms.
 #define CONFIG_CLICK_LATENCY_US 255'000
 
-// "Window" between clicks, as (not) defined by LIS35DE documentation. Must
-// range from 0ms to 255ms with a step of 1ms.
+// The maximum time after the latency period during which a double click event
+// can be start to be registered. The window does not limit when the double
+// click registration finishes, as long as it finishes within the time limit.
+// Must range from 0ms to 255ms with a step of 1ms.
 #define CONFIG_CLICK_WINDOW_US 255'000
