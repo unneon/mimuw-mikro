3f557ec76cbd17061bb6252c961fd82b903e9a71
Mateusz Cegiełka <mateusz@cegla.net>
2025-02-26 09:52:40 +0100

    Style unnecessary config prefix

--- src/config.h
+++ src/config.h
@@ -3,19 +3,19 @@
 // The threshold of acceleration along the Z axis required to start and finish
 // registering a click, measured in milli-g (1/1000th of 9.81m/s^2). Must range
 // from 0.5g to 7.5g with a step of 0.5g.
-#define CONFIG_CLICK_THRESHOLD_MG 3'500
+#define CLICK_THRESHOLD_MG 3'500
 
 // The maximum time between the acceleration exceeding the threshold value and
 // acceleration then falling below the threshold value required to register any
 // click event. Must range from 0ms to 127.5ms with a step of 0.5ms.
-#define CONFIG_CLICK_TIMELIMIT_US 127'500
+#define CLICK_TIMELIMIT_US 127'500
 
 // The minimal duration after which a double click event can be registered
 // after a single click event. Must range from 0ms to 255ms with a step of 1ms.
-#define CONFIG_CLICK_LATENCY_US 255'000
+#define CLICK_LATENCY_US 255'000
 
 // The maximum time after the latency period during which a double click event
 // can be start to be registered. The window does not limit when the double
 // click registration finishes, as long as it finishes within the time limit.
 // Must range from 0ms to 255ms with a step of 1ms.
-#define CONFIG_CLICK_WINDOW_US 255'000
+#define CLICK_WINDOW_US 255'000
--- src/main.c
+++ src/main.c
@@ -33,10 +33,10 @@
 #define TIMER_PSC 64'000
 #define TIMER_PERIOD_TICKS 750
 
-static_assert(CONFIG_CLICK_THRESHOLD_MG % 500 == 0 && CONFIG_CLICK_THRESHOLD_MG >= 500 && CONFIG_CLICK_THRESHOLD_MG <= 7'500, "Click threshold must range from 0.5g to 7.5g with a step of 0.5g.");
-static_assert(CONFIG_CLICK_TIMELIMIT_US % 500 == 0 && CONFIG_CLICK_TIMELIMIT_US >= 0 && CONFIG_CLICK_TIMELIMIT_US <= 127'500, "Click time limit must range from 0ms to 127.5ms with a step of 0.5ms.");
-static_assert(CONFIG_CLICK_LATENCY_US % 1'000 == 0 && CONFIG_CLICK_LATENCY_US >= 0 && CONFIG_CLICK_LATENCY_US <= 255'000, "Click latency must range from 0ms to 255ms with a step of 1ms.");
-static_assert(CONFIG_CLICK_WINDOW_US % 1'000 == 0 && CONFIG_CLICK_WINDOW_US >= 0 && CONFIG_CLICK_WINDOW_US <= 255'000, "Click window must range from 0ms to 255ms with a step of 1ms.");
+static_assert(CLICK_THRESHOLD_MG % 500 == 0 && CLICK_THRESHOLD_MG >= 500 && CLICK_THRESHOLD_MG <= 7'500, "Click threshold must range from 0.5g to 7.5g with a step of 0.5g.");
+static_assert(CLICK_TIMELIMIT_US % 500 == 0 && CLICK_TIMELIMIT_US >= 0 && CLICK_TIMELIMIT_US <= 127'500, "Click time limit must range from 0ms to 127.5ms with a step of 0.5ms.");
+static_assert(CLICK_LATENCY_US % 1'000 == 0 && CLICK_LATENCY_US >= 0 && CLICK_LATENCY_US <= 255'000, "Click latency must range from 0ms to 255ms with a step of 1ms.");
+static_assert(CLICK_WINDOW_US % 1'000 == 0 && CLICK_WINDOW_US >= 0 && CLICK_WINDOW_US <= 255'000, "Click window must range from 0ms to 255ms with a step of 1ms.");
 
 // All accesses to the global variables happen in interrupt handlers, with one
 // exception. The `global_sleep_semaphore` variable is accessed in
@@ -203,11 +203,11 @@ static constexpr unsigned char ACCELEROMETER_INITSEQ_2[] = {
 
 static constexpr unsigned char ACCELEROMETER_INITSEQ_3[] = {
     LIS35DE_CLICKTHSYX | LIS35DE_AUTOINCREMENT,
-    ((CONFIG_CLICK_THRESHOLD_MG / 500) << 4) | (CONFIG_CLICK_THRESHOLD_MG / 500),
-    CONFIG_CLICK_THRESHOLD_MG / 500,
-    CONFIG_CLICK_TIMELIMIT_US / 500,
-    CONFIG_CLICK_LATENCY_US / 1'000,
-    CONFIG_CLICK_WINDOW_US / 1'000,
+    ((CLICK_THRESHOLD_MG / 500) << 4) | (CLICK_THRESHOLD_MG / 500),
+    CLICK_THRESHOLD_MG / 500,
+    CLICK_TIMELIMIT_US / 500,
+    CLICK_LATENCY_US / 1'000,
+    CLICK_WINDOW_US / 1'000,
 };
 
 static constexpr unsigned char ACCELEROMETER_INITSEQ_4[] = {
